52ee0f4ab2c9dbc18d0867d09b6cecacd1c5576b
YeongGwan Jeong <38877902+swarthyPig@users.noreply.github.com>
2019-09-03 02:56:18 +0900

    Delete 두_정수_사이의_합.py

--- Algorithm_problems/Python/두_정수_사이의_합.py
+++
@@ -1,9 +0,0 @@
-def solution(a, b):
-
-    if a == b: # a,b가 같으면 a 리턴
-        return a
-
-    elif a > b: # 시작 숫자가 끝 숫자보다 크면 swap
-        a,b = b,a
-
-    return sum(range(a,b+1)) # sum 함수를 사용하여 덧셈
