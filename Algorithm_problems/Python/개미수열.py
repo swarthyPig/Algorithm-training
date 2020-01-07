5d574e785e1320985667087368493aed896dbb2e
YeongGwan Jeong <38877902+swarthyPig@users.noreply.github.com>
2020-01-08 02:21:42 +0900

    Update 개미수열.py

--- Algorithm_problems/Python/개미수열.py
+++ Algorithm_problems/Python/개미수열.py
@@ -10,26 +10,23 @@ def antArray(n):
 
     for i in range(n):
         result = []
-        count = 0
-        finalTemp = 0 # 회전이 끝났을 때 판단할 count
+        count = 0 # 숫자가 반복될 때의 갯수 
         num = 1 # 판단할 숫자
 
         for j in range(len(mylist)):
 
             if mylist[j] == num: # 숫자가 반복될 때
                 count += 1
-                finalTemp = count # 숫자가 반복된 상태에서 끝났을 때 의 count
 
             if mylist[j] != num: # 숫자가 다를 때
                 result.append(num)
                 result.append(count)
                 num = mylist[j] # 판단할 숫자를 변경
-                count = 1 # 매칭이 되지 않기 때문에 count를 1로 초기화
-                finalTemp = count # 다른 상태에서 끝났을때의 count
+                count = 1 # 숫자가 다를 때 매칭이 되지 않기 때문에 count를 1로 초기화
 
-            if j == len(mylist) - 1: # 끝날 때
+            if j == len(mylist) - 1: # 끝날 때(배열의 마지막 인덱스 처리)
                 result.append(num)
-                result.append(finalTemp)
+                result.append(count)
 
         print(f"{i+2}번째:{result}")
         mylist = result[:]
